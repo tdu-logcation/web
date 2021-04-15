7e498b6de0c30302a235d0c966d7ee8162f7bf36
Yuto Watanabe <yuto.w51942@gmail.com>
2021-04-15 23:30:31 +0900

    _appを追加

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import type {AppProps} from 'next/app';
+import {ChakraProvider} from '@chakra-ui/react';
+import {RecoilRoot} from 'recoil';
+import React from 'react';
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <RecoilRoot>
+      <ChakraProvider>
+        <Component {...pageProps} />;
+      </ChakraProvider>
+    </RecoilRoot>
+  );
+}
+
+export default MyApp;
