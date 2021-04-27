d913998c16d1d52acf240bf5eed097725e724092
Yuto Watanabe <yuto.w51942@gmail.com>
2021-04-27 16:47:40 +0900

    デザイン修正

--- components/History.tsx
+++ components/History.tsx
@@ -9,12 +9,14 @@ import {
   Tbody,
   Flex,
   Text,
+  Box,
 } from '@chakra-ui/react';
 import {useRecoilState} from 'recoil';
 import {logState} from '../utils/recoilAtoms';
 import {parseQrData} from '../utils/logUtil';
 import {useTable, useSortBy} from 'react-table';
 import {IoArrowUpOutline, IoArrowDownOutline} from 'react-icons/io5';
+import * as colors from '../utils/colors';
 
 export const History = () => {
   const [log] = useRecoilState(logState);
@@ -27,7 +29,6 @@ export const History = () => {
         building: parsedData.buildingNumber,
         floors: parsedData.floorNumber,
         rooms: parsedData.roomNumber,
-        seat: parsedData.seatNumber,
       };
     });
   }, []);
@@ -52,10 +53,6 @@ export const History = () => {
         Header: '部屋番号',
         accessor: 'rooms',
       },
-      {
-        Header: '席番',
-        accessor: 'seat',
-      },
     ],
     []
   );
@@ -71,49 +68,58 @@ export const History = () => {
   return (
     <React.Fragment>
       <Center>
-        <Table
-          {...getTableProps()}
-          variant="striped"
-          colorScheme="gray"
-          size="sm"
-        >
-          <Thead>
-            {headerGroups.map(headerGroup => (
-              <Tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    <Center>
-                      <Flex>
-                        <Text>{column.render('Header')}</Text>
-                        <Text>
-                          {column.isSorted ? (
-                            column.isSortedDesc ? (
-                              <IoArrowUpOutline />
-                            ) : (
-                              <IoArrowDownOutline />
-                            )
-                          ) : null}
-                        </Text>
-                      </Flex>
-                    </Center>
-                  </Th>
-                ))}
-              </Tr>
-            ))}
-          </Thead>
-          <Tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-              prepareRow(row);
-              return (
-                <Tr {...row.getRowProps()}>
-                  {row.cells.map(cell => (
-                    <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>
+        <Box margin="1rem .75rem 1rem .75rem">
+          <Table
+            {...getTableProps()}
+            variant="striped"
+            colorScheme="gray"
+            size="sm"
+          >
+            <Thead>
+              {headerGroups.map(headerGroup => (
+                <Tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map(column => (
+                    <Th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      padding=".5rem 0 .5rem 0"
+                      backgroundColor={colors.mainPrimary}
+                      color={colors.textPrimary}
+                    >
+                      <Center>
+                        <Flex>
+                          <Text>{column.render('Header')}</Text>
+                          <Text>
+                            {column.isSorted ? (
+                              column.isSortedDesc ? (
+                                <IoArrowUpOutline />
+                              ) : (
+                                <IoArrowDownOutline />
+                              )
+                            ) : null}
+                          </Text>
+                        </Flex>
+                      </Center>
+                    </Th>
                   ))}
                 </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
+              ))}
+            </Thead>
+            <Tbody {...getTableBodyProps()}>
+              {rows.map(row => {
+                prepareRow(row);
+                return (
+                  <Tr {...row.getRowProps()}>
+                    {row.cells.map(cell => (
+                      <Td {...cell.getCellProps()} textAlign="center">
+                        {cell.render('Cell')}
+                      </Td>
+                    ))}
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </Box>
       </Center>
     </React.Fragment>
   );
