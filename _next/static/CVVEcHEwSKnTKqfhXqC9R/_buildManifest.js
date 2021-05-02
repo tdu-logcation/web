53d739d529765c7e33a9c340f1ac968888916105
yuto51942 <yuto51942@users.noreply.github.com>
2021-05-02 11:35:58 +0000

    deploy: 03a72fc03085d153041473a1d9e2a4aa140b92c7

--- _next/static/CVVEcHEwSKnTKqfhXqC9R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-53f767d79dae7c460e8f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9503fd1494f00d5239d2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
