5d1735c59e80c5258df80ff6e5e5d641c0669c84
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-05-03 14:06:22 +0900

    feat: systemColorMode Detectionの設定追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,11 @@ import type {AppProps} from 'next/app';
 import {ChakraProvider} from '@chakra-ui/react';
 import {RecoilRoot} from 'recoil';
 import React from 'react';
+import theme from "../utils/theme";
 
 const MyApp = ({Component, pageProps}: AppProps) => (
   <RecoilRoot>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   </RecoilRoot>
