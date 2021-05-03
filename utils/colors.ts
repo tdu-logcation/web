ad3ea6bc4defc7d33094ded026eaac4ebdd9c933
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-05-03 16:09:30 +0900

    chore: サンプルとして各Colorの明暗を反転

--- utils/colors.ts
+++ utils/colors.ts
@@ -5,18 +5,18 @@
  */
 
 export const dark = {
-  mainPrimary: '#bdd7ee',
-  mainSecondly: '#406b94',
+  mainPrimary: '#bdd7ee', // TODO: 調整
+  mainSecondly: '#406b94', // TODO: 調整
 
-  textPrimary: '#2b2b2b',
-  textSecondly: '#4a4a4a',
-  textTertiary: '#fafafa',
+  textPrimary: '#d6d6d6',
+  textSecondly: '#b5b5b5',
+  textTertiary: '#050505',
 
-  buttonPrimary: '#eebdc0',
-  buttonSecondly: '#f2f2f2',
-  buttonIconSecondly: '#4a4a4a',
+  buttonPrimary: '#eebdc0', // TODO: 調整
+  buttonSecondly: '#0f0f0f',
+  buttonIconSecondly: '#b5b5b5',
 
-  divider: '#f2f2f2',
+  divider: '#0f0f0f',
 };
 export const light = {
   mainPrimary: '#bdd7ee',
