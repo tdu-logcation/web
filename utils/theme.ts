33416002d6a33aa9fb78562b3a6a435e248382cb
Yuto Watanabe <yuto.w51942@gmail.com>
2021-05-04 18:03:07 +0900

    Update utils/theme.ts
    Co-authored-by: Taiyu Yoshizawa <taiyu31@gmail.com>

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,7 +1,7 @@
 import {extendTheme, ThemeConfig} from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: 'light',
+  initialColorMode: 'system',
   useSystemColorMode: false,
 };
 
