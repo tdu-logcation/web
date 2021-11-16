8c8792fb5ae5947d1723834a61bf0aebb065e9c8
Yuto Watanabe <yuto.w51942@gmail.com>
2021-11-16 14:58:22 +0900

    ユーザ名変更機能の追加

--- components/settings/UserInfo.tsx
+++ components/settings/UserInfo.tsx
@@ -19,18 +19,29 @@ import {
   useDisclosure,
   UnorderedList,
   ListItem,
+  Input,
 } from '@chakra-ui/react';
 import {colors} from '../../utils/colors';
 import {IoPersonCircleOutline} from 'react-icons/io5';
 import {useRecoilValue} from 'recoil';
 import {userInfo} from '../../utils/recoilAtoms';
 import {formatDate} from '../../utils/formatUtil';
+import React from 'react';
+import useRename from '../../hooks/useRename';
 
 const UserInfo = () => {
   const _userInfo = useRecoilValue(userInfo);
   const {onCopy} = useClipboard(_userInfo?.id);
   const toast = useToast();
   const {isOpen, onOpen, onClose} = useDisclosure();
+  const [userName, setUserName] = React.useState('');
+  const [userNameState, setUserNameState] = React.useState(false);
+  const rename = useRename();
+  const {
+    isOpen: isOpenChangeName,
+    onOpen: onOpenChangeName,
+    onClose: onCloseChangeName,
+  } = useDisclosure();
 
   const handleChange = () => {
     onCopy();
@@ -38,6 +49,22 @@ const UserInfo = () => {
       title: 'IDをコピーしました',
       status: 'success',
     });
+  };
+
+  const inputUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserName(event.target.value);
+  };
+
+  const changeUserName = () => {
+    if (userName.length === 0) {
+      setUserNameState(true);
+      return;
+    }
+    rename(userName);
+
+    onCloseChangeName();
+    setUserNameState(true);
+    setUserName('');
   };
 
   return (
@@ -75,8 +102,47 @@ const UserInfo = () => {
           </ModalFooter>
         </ModalContent>
       </Modal>
+      <Modal
+        isOpen={isOpenChangeName}
+        onClose={onCloseChangeName}
+        isCentered
+        motionPreset="slideInBottom"
+        size="sm"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>ユーザ名変更</ModalHeader>
+          <ModalCloseButton size="lg" />
+          <ModalBody>
+            <Input
+              marginTop="1rem"
+              placeholder="新しいユーザ名"
+              value={userName}
+              onChange={inputUserName}
+              isInvalid={userNameState}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              backgroundColor={colors('mainPrimary')}
+              variant="ghost"
+              onClick={changeUserName}
+              marginRight=".5rem"
+            >
+              変更
+            </Button>
+            <Button
+              color={colors('textPrimary')}
+              variant="ghost"
+              onClick={onCloseChangeName}
+            >
+              閉じる
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       <ListIcon as={IoPersonCircleOutline} color={colors('mainSecondly')} />
-      ユーザ情報
+      ユーザ関連
       <Flex marginLeft="1rem" marginY=".5rem">
         <Button
           backgroundColor={colors('mainPrimary')}
@@ -87,6 +153,16 @@ const UserInfo = () => {
           }}
         >
           ユーザ情報
+        </Button>
+        <Button
+          backgroundColor={colors('mainPrimary')}
+          marginX=".5rem"
+          size="sm"
+          onClick={() => {
+            onOpenChangeName();
+          }}
+        >
+          名前変更
         </Button>
         <Button
           backgroundColor={colors('mainPrimary')}
